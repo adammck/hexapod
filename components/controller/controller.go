71ce245ee160943587b1a9b26b56a824076c05c1
Adam Mckaig <adam.mckaig@gmail.com>
2016-02-07 10:55:17 -0500

    Update controller to set target position

--- components/controller/controller.go
+++ components/controller/controller.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/adammck/hexapod"
 	"github.com/adammck/hexapod/math3d"
 	"github.com/adammck/sixaxis"
@@ -10,14 +10,8 @@ import (
 )
 
 const (
-
-	// Maximum movement speed.
-	// TODO: What the hell is the unit here?
-	moveSpeed = 1.5
-
-	// The maximum speed to rotate (i.e. when the right stick is fully pressed)
-	// in degrees per loop.
-	rotationSpeed = 0.8
+	moveSpeed = 10.0
+	rotSpeed  = 0.8
 )
 
 type Controller struct {
@@ -30,51 +24,43 @@ func New(r io.Reader) *Controller {
 	}
 }
 
-// TODO: Log
 func (c *Controller) Boot() error {
 	go c.sa.Run()
 	return nil
 }
 
-// TODO: Update the state of the hexapod based on the state of the controller.
 func (c *Controller) Tick(now time.Time, state *hexapod.State) error {
 
-	// Rotate with the right stick
-	if c.sa.RightStick.X != 0 {
-		state.TargetRotation += (float64(c.sa.RightStick.X) / 127.0) * rotationSpeed
+	// Set the target position by creating a vector from the left stick (for the
+	// X/Z axes), and the dpad (for the Z axis or ground clearance), and adding
+	// it to the current position.
+
+	v := math3d.ZeroVector3
+	if c.sa.LeftStick.X > 2.0 {
+		v.X = (float64(c.sa.LeftStick.X) / 127.0) * moveSpeed
 	}
-
-	// How much the origin should move this frame. Default is zero, but this
-	// it mutated (below) by the various buttons.
-	vecMove := math3d.MakeVector3(0, 0, 0)
-
-	if c.sa.LeftStick.X != 0 {
-		vecMove.X = (float64(c.sa.LeftStick.X) / 127.0) * moveSpeed
+	if c.sa.LeftStick.Y > 2.0 {
+		v.Z = (float64(-c.sa.LeftStick.Y) / 127.0) * moveSpeed
 	}
-
-	if c.sa.LeftStick.Y != 0 {
-		vecMove.Z = (float64(-c.sa.LeftStick.Y) / 127.0) * moveSpeed
-	}
-
-	// Move the origin up (away from the ground) with the dpad. This alters
-	// the gait by keeping the body up in the air. It looks weird but works.
 	if c.sa.Up > 0 {
-		vecMove.Y = +2
+		v.Y = 2
 	}
-
 	if c.sa.Down > 0 {
-		vecMove.Y = -2
+		v.Y = 2
+	}
+	if !v.Zero() {
+		state.TargetPosition = *state.Position.Add(v)
 	}
 
-	// Update the target position, if it's changed.
-	if !vecMove.Zero() {
-		mx := *math3d.MakeMatrix44(state.Position, *math3d.MakeSingularEulerAngle(math3d.RotationHeading, state.Rotation))
-		state.TargetPosition = vecMove.MultiplyByMatrix44(mx)
+	// Rotate with the right stick
+	if c.sa.RightStick.X > 2.0 {
+		r := (float64(c.sa.RightStick.X) / 127.0) * rotSpeed
+		state.TargetRotation = state.Rotation + r
 	}
 
 	// At any time, pressing start shuts down the hex.
 	if c.sa.Start {
-		fmt.Println("Pressed START, shutting down")
+		log.Warn("Pressed START, shutting down")
 		state.Shutdown = true
 	}
 
